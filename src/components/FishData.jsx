a1514dd1194ff4e445631f6fd5a87b9f557328a7
Rekk <nyanpyaetun4105@gmail.com>
2025-12-23 00:18:44 +0630

    Add files via upload

---
+++ src/components/FishData.jsx
@@ -0,0 +1,176 @@
+import fish1 from "../assets/Collection/1.jpg";
+import fish2 from "../assets/Collection/2.jpg";
+import fish3 from "../assets/Collection/3.jpg";
+import fish4 from "../assets/Collection/4.jpg";
+import fish5 from "../assets/Collection/5.jpg";
+import fish6 from "../assets/Collection/6.jpg";
+import fish7 from "../assets/Collection/7.jpg";
+import fish8 from "../assets/Collection/8.jpg";
+import fish9 from "../assets/Collection/9.jpg";
+import fish10 from "../assets/Collection/10.jpg";
+import fish11 from "../assets/Collection/11.jpg";
+import fish12 from "../assets/Collection/12.jpg";
+import fish13 from "../assets/Collection/13.jpg";
+import fish14 from "../assets/Collection/14.jpg";
+import fish15 from "../assets/Collection/15.jpg";
+import fish16 from "../assets/Collection/16.jpg";
+import fish17 from "../assets/Collection/17.jpg";
+import fish18 from "../assets/Collection/18.jpg";
+import fish19 from "../assets/Collection/19.jpg";
+import fish20 from "../assets/Collection/20.jpg";
+import fish21 from "../assets/Collection/21.jpg";
+import fish22 from "../assets/Collection/22.jpg";
+import fish23 from "../assets/Collection/23.jpg";
+import fish24 from "../assets/Collection/24.jpg";
+// ================== FISH DATA ==================
+export const fishList = [
+  // BETTA
+  {
+    name: "Imbellis Green (Wild)",
+    category: "Betta",
+    slug: "imbellis-green-wild",
+    image: fish1,
+  },
+  {
+    name: "Mahachai Green (Wild)",
+    category: "Betta",
+    slug: "mahachai-green-wild",
+    image: fish2,
+  },
+  {
+    name: "Mahachai Blue (Wild)",
+    category: "Betta",
+    slug: "mahachai-blue-wild",
+    image: fish3,
+  },
+  {
+    name: "Alien Blue (Wild)",
+    category: "Betta",
+    slug: "alien-blue-wild",
+    image: fish4,
+  },
+  {
+    name: "Alien Green (Wild)",
+    category: "Betta",
+    slug: "alien-green-wild",
+    image: fish5,
+  },
+  {
+    name: "Xelophane (HMPK)",
+    category: "Betta",
+    slug: "xelophane-hmpk",
+    image: fish6,
+  },
+  {
+    name: "Snow Dragon (HMPK)",
+    category: "Betta",
+    slug: "snow-dragon-hmpk",
+    image: fish7,
+  },
+  {
+    name: "Samurai / Black Mamba (HMPK)",
+    category: "Betta",
+    slug: "samurai-black-mamba-hmpk",
+    image: fish8,
+  },
+  {
+    name: "Avatar Blue (HMPK)",
+    category: "Betta",
+    slug: "avatar-blue-hmpk",
+    image: fish9,
+  },
+  {
+    name: "Imbellis Blue (Wild)",
+    category: "Betta",
+    slug: "imbellis-blue-wild",
+    image: fish10,
+  },
+  {
+    name: "Yellow Multi (HMPK)",
+    category: "Betta",
+    slug: "yellow-multi-hmpk",
+    image: fish11,
+  },
+  {
+    name: "Fighting Plakat (Wild)",
+    category: "Betta",
+    slug: "fighting-plakat-wild",
+    image: fish12,
+  },
+  {
+    name: "Fancy Copper (HMPK)",
+    category: "Betta",
+    slug: "fancy-copper-hmpk",
+    image: fish13,
+  },
+  {
+    name: "Crown Tail",
+    category: "Betta",
+    slug: "crown-tail",
+    image: fish14,
+  },
+  {
+    name: "Dumbo",
+    category: "Betta",
+    slug: "dumbo",
+    image: fish15,
+  },
+  {
+    name: "Halfmoon",
+    category: "Betta",
+    slug: "halfmoon",
+    image: fish16,
+  },
+
+  // CHANNA
+  {
+    name: "Channa Guacha",
+    category: "Channa",
+    slug: "channa-guacha",
+    image: fish17,
+  },
+  {
+    name: "Channa Blue Pulchra",
+    category: "Channa",
+    slug: "channa-blue-pulchra",
+    image: fish18,
+  },
+  {
+    name: "Channa Fire & Ice",
+    category: "Channa",
+    slug: "channa-fire-ice",
+    image: fish19,
+  },
+  {
+    name: "Channa Golden Cobra",
+    category: "Channa",
+    slug: "channa-golden-cobra",
+    image: fish20,
+  },
+  {
+    name: "Channa Barca",
+    category: "Channa",
+    slug: "channa-barca",
+    image: fish21,
+  },
+
+  // FLOWER HORN
+  {
+    name: "Flower Horn Golden Base",
+    category: "Flower Horn",
+    slug: "flower-horn-golden-base",
+    image: fish22,
+  },
+  {
+    name: "Flower Horn (Super Red Dragon)",
+    category: "Flower Horn",
+    slug: "flower-horn-super-red-dragon",
+    image: fish23,
+  },
+  {
+    name: "Red Solid (HMPK)",
+    category: "Betta",
+    slug: "red-solid-hmpk",
+    image: fish24,
+  },
+];
